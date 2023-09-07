c00b2922650f99d20677691f0439bda09654e73a
romansergeevich <roma.borozenec@yandex.ru>
2023-09-07 15:37:16 +0300

    модуль 1,задача 5 усечение строки

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,20 @@
 function checkSpam(str) {
-  // ваш код...
+
+
+  str = str.toUpperCase();
+
+  if (str.indexOf('1XBET')!=-1) {
+    return true;
+
+  }
+  else if (str.indexOf('XXX') != -1) {
+    return true;
+
+  }
+
+  else
+  {
+    return false;
+  }
+
 }
